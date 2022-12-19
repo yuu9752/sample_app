47df0cb69c3ae11733fc080df93d6a2814d879e6
EC2 Default User <ec2-user@ip-172-31-32-189.ap-northeast-1.compute.internal>
2022-12-19 04:08:12 +0000

    アプリ-3

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,26 @@
+class ListsController < ApplicationController
+  def new
+    @list = List.new
+  end
+  
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to'/top'
+  end
+
+  def index
+    @list = List.find(params[:id])
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  private
+  def list_params
+    params.require(:list).permit(:title,:body)
+  end
+end
